1e480ccac23d90c06658f6066909ae66aef5471c
André Fellipe Amorim <andrefellipeamorim@gmail.com>
2018-06-12 21:03:18 -0300

    add js effect
    Learning to use some js;

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function swingItemC() {
+	document.getElementById('chapeu').className += "img-responsive ficons animated swing infinite";
+}
+
+function stopSwingC() {
+	document.getElementById('chapeu').className = "img-responsive ficons";	
+}
+
+function swingItemM() {
+	document.getElementById('mala').className += "img-responsive ficons animated swing infinite";
+}
+
+function stopSwingM() {
+	document.getElementById('mala').className = "img-responsive ficons";	
+}
+
+function swingItemD() {
+	document.getElementById('diploma').className += "img-responsive ficons animated swing infinite";
+}
+
+function stopSwingD() {
+	document.getElementById('diploma').className = "img-responsive ficons";	
+}
